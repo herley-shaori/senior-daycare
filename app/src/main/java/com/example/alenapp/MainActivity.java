87992d748681d8e486ebd64f912a21aba62283a2
herley.shaori1 <herley.shaori@gmail.com>
2021-05-08 23:19:29 +0700

    Processing custom Support Action Bar.

--- app/src/main/java/com/example/alenapp/MainActivity.java
+++ app/src/main/java/com/example/alenapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.alenapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        ActionBar supportActionBar = this.getSupportActionBar();
+        supportActionBar.setDisplayShowCustomEnabled(true);
+        supportActionBar.setIcon(R.drawable.senja_apps);
+
+
+//        ActionBar actionBar = this.getActionBar();
+//        actionBar.setDisplayHomeAsUpEnabled(true);
+//        actionBar.setIcon(R.drawable.senja_apps.);
+//        this.getActionBar().setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.activity_main);
     }
 }
