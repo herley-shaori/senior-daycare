8fc87b51b1aa89742c5c037c10668eb395610cfc
herley.shaori1 <herley.shaori@gmail.com>
2021-05-09 12:08:51 +0700

    Delivering first stage.

--- app/src/main/java/com/example/alenapp/MainActivity.java
+++ app/src/main/java/com/example/alenapp/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.alenapp;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatePickerDialog datePickerDialog;
+    private SimpleDateFormat dateFormatter;
+    private TextView tvDateResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +38,82 @@ public class MainActivity extends AppCompatActivity {
 
         // Action bar background color.
         supportActionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFFFFF")));
-
         setContentView(R.layout.activity_main);
+
+        // Button action handler.
+        AppCompatButton buttonGo = findViewById(R.id.go_button);
+        buttonGo.setOnClickListener(actionNotAvailable());
+        findViewById(R.id.senior_daycare).setOnClickListener(actionNotAvailable());
+        findViewById(R.id.antar_jemput).setOnClickListener(actionNotAvailable());
+        findViewById(R.id.event).setOnClickListener(actionNotAvailable());
+        findViewById(R.id.home).setOnClickListener(actionNotAvailable());
+        findViewById(R.id.my_order).setOnClickListener(actionNotAvailable());
+        findViewById(R.id.elite_rewards).setOnClickListener(actionNotAvailable());
+
+        // Daycare schedule button handler.
+        this.dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        this.tvDateResult = findViewById(R.id.daycare_date);
+        AppCompatButton buttonPickADate = findViewById(R.id.date_button);
+        buttonPickADate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shorDateCalendarDialog();
+            }
+        });
+    }
+
+    /**
+     * Tombol aksi implementasi fitur belum tersedia.
+     * @return
+     */
+    private View.OnClickListener actionNotAvailable() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Fitur belum tersedia.",
+                        Toast.LENGTH_LONG).show();
+            }
+        };
+    }
+
+    /**
+     * Tampilkan tanggal.
+     */
+    private void shorDateCalendarDialog() {
+        /**
+         * Calendar untuk mendapatkan tanggal sekarang
+         */
+        Calendar newCalendar = Calendar.getInstance();
+
+        /**
+         * Initiate DatePicker dialog
+         */
+        datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
+                /**
+                 * Method ini dipanggil saat kita selesai memilih tanggal di DatePicker
+                 */
+
+                /**
+                 * Set Calendar untuk menampung tanggal yang dipilih
+                 */
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+
+                /**
+                 * Update TextView dengan tanggal yang kita pilih
+                 */
+                tvDateResult.setText(dateFormatter.format(newDate.getTime()));
+            }
+
+        }, newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        /**
+         * Tampilkan DatePicker dialog
+         */
+        datePickerDialog.show();
     }
 }
